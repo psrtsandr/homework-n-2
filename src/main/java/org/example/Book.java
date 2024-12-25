d0bf71dec8faa76e4509486f6d5198536e5bfd3f
Andrey Pisarets <pisarets.andrey@gmail.com>
2024-12-25 23:29:15 +0300

    Modified methods

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -2,23 +2,43 @@ package org.example;
 
 import java.util.Objects;
 
-public record Book(BookTitle title, BookAuthor author, BookPublicationYear publicationYear) {
+public record Book(BookTitle title, BookAuthor author, BookPublicationYear publicationYear, BookPagesCount pagesCount) {
+    public Book {
+        if (title == null) {
+            throw new IllegalArgumentException("Book title cannot be null");
+        }
+        if (author == null) {
+            throw new IllegalArgumentException("Book author cannot be null");
+        }
+        if (publicationYear == null) {
+            throw new IllegalArgumentException("Book publication year cannot be null");
+        }
+        if (pagesCount == null) {
+            throw new IllegalArgumentException("Book pages count cannot be null");
+        }
+    }
+
     @Override
     public String toString() {
-        return "Book (Title: %1$s, Author: %2$s, Publication year: %3$s".formatted(title, author, publicationYear);
+        return "Book (Title: %1$s, Author: %2$s, Publication year: %3$s, Pages count: %4$s"
+                .formatted(title, author, publicationYear, pagesCount);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Book(BookTitle otherTitle, BookAuthor otherAuthor, BookPublicationYear otherPublicationYear))) return false;
-        return Objects.equals(title(), otherTitle) &&
-            Objects.equals(author(), otherAuthor) &&
-            Objects.equals(publicationYear(), otherPublicationYear);
+        return (o instanceof Book(BookTitle otherTitle,
+                                  BookAuthor otherAuthor,
+                                  BookPublicationYear otherPublicationYear,
+                                  BookPagesCount otherPagesCount)) &&
+                Objects.equals(title, otherTitle) &&
+                Objects.equals(author, otherAuthor) &&
+                Objects.equals(publicationYear, otherPublicationYear) &&
+                Objects.equals(pagesCount, otherPagesCount);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title(), author(), publicationYear());
+        return Objects.hash(title, author, publicationYear, pagesCount);
     }
 }
 
