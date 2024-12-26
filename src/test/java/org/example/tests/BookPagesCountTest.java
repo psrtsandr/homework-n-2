d7bc2e5258d1a8804c05cc411e65a2f10ed3fb40
Andrey Pisarets <pisarets.andrey@gmail.com>
2024-12-26 13:25:29 +0300

    Tested compareTo() method

--- src/test/java/org/example/tests/BookPagesCountTest.java
+++ src/test/java/org/example/tests/BookPagesCountTest.java
@@ -10,4 +10,25 @@ public class BookPagesCountTest {
     public void testConstructorThrowsWhenValueIsNegative() {
         assertThrows(IllegalArgumentException.class, () -> new BookPagesCount(-1));
     }
+
+    @Test
+    public void testCompareTo() {
+        var a = new BookPagesCount(1);
+        var b = new BookPagesCount(2);
+        var expected = -1;
+        var actual = a.compareTo(b);
+        assertEquals(expected, actual);
+
+        a = new BookPagesCount(2);
+        b = new BookPagesCount(1);
+        expected = 1;
+        actual = a.compareTo(b);
+        assertEquals(expected, actual);
+
+        a = new BookPagesCount(1);
+        b = new BookPagesCount(1);
+        expected = 0;
+        actual = a.compareTo(b);
+        assertEquals(expected, actual);
+    }
 }
