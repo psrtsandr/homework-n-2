5b047462b51aa5d7cecdf087d7edefb87cc6066f
Andrey Pisarets <pisarets.andrey@gmail.com>
2024-12-26 16:18:19 +0300

    Added some demo code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Student> students = StudentMockRepository.getStudents();
+        students.stream()
+                .peek(s -> System.out.printf("%s\nStudent's books:\n%s%n\n",
+                        s,
+                        s.getBooks().stream()
+                                .sorted(Comparator.comparing(Book::publicationYear))
+                                .map(String::valueOf)
+                                .collect(Collectors.joining("\n"))))
+                .flatMap(s -> s.getBooks().stream())
+                .sorted(Comparator.comparing(Book::pagesCount))
+                .distinct()
+                .filter(b -> b.publicationYear().value() > 2000)
+                .mapToInt(b -> b.publicationYear().value())
+                .limit(3)
+                .findFirst()
+                .ifPresentOrElse(y -> System.out.printf("Found book publication year: %d\n", y), () -> System.out.println("Book not found"));
     }
 }
